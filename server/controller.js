cbea3bfb66ec518e8eef437b93af01981992755b
Hayden Sweet <haydensweet22@gmail.com>
2022-05-11 12:58:04 -0600

    initial

---
+++ server/controller.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+const Sequelize = require("sequelize");
+
+//This is the same syntax as CONNECTION_STRING from our .env file
+const {CONNECTION_STRING} = process.env; //Destructuring CONNECTION_STRING from our .env file
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect:'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
+        }
+    }
+})
+
+const userId = 4;
+const clientId = 3;
+
+module.exports = {
+    getUserInfo: (req, res) => {
+        sequelize.query(`SELECT * FROM cc_clients AS c
+        JOIN cc_users AS u ON c.user_id = u.user_id
+        WHERE u.user_id = ${userId}`)
+        .then((dbRes) => {
+            res.status(200).send(dbRes[0]); //Postgres will always put your data at hte [0] index of dbRes
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    },
+
+    updateUserInfo: (req, res) => {
+        let {
+            firstName,
+            lastName,
+            phoneNumber,
+            email,
+            address,
+            city,
+            state,
+            zipCode
+        } = req.body;
+
+        sequelize.query(`UPDATE cc_users SET first_name='${firstName}',
+        last_name='${lastName}',
+        email='${email}',
+        phone_number=${phoneNumber}
+        WHERE user_id=${userId};
+        
+        UPDATE cc_clients SET address='${address}',
+        city='${city}',
+        state='${state}',
+        zip_code=${zipCode}
+        WHERE user_id=${userId};`)
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+}
